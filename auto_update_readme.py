e672fa2903de9f7ef7ff9c8a105aa9ec887ab9fb
Yohann Boniface <edhyjox@gmail.com>
2021-07-18 18:49:33 +0200

    [fix] Moving base and actual to svg folder

--- auto_update_readme.py
+++ auto_update_readme.py
@@ -59,7 +59,7 @@ commit_avg = ''.join(c for c in commit_avg if c.isdigit() or c == '.')
 svg_vars['streak'] = streak
 svg_vars['cmt_avg'] = commit_avg
 
-with open("base.svg") as f:
+with open("svg/base.svg") as f:
     svg = f.read()
 
 for k, v in svg_vars.items():
@@ -67,5 +67,5 @@ for k, v in svg_vars.items():
 
 svg = svg.replace('<p>empty</p>', '')
 
-with open("readme.svg", "w") as f:
+with open("svg/readme.svg", "w") as f:
     f.write(svg)
