63a0d80c70c188c57a5ab5978a757aea1a7d5bdb
sigmanificient <edhyjox@gmail.com>
2023-03-16 23:48:34 +0100

    :wrench: Update flake to be system independent

--- flake.nix
+++ flake.nix
@@ -1,19 +1,21 @@
 {
   description = "Sigmachine Readme";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
-  outputs = { self, nixpkgs }:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in
-  {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      venvDir = "venv";
-      buildInputs = [
-        pkgs.gnumake
-        pkgs.python310Full
-        pkgs.python310Packages.venvShellHook
-      ];
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, utils } @inputs:
+    utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = mkShell {
+          venvDir = "venv";
+          buildInputs = [
+            pkgs.gnumake
+            pkgs.python310Full
+            pkgs.python310Packages.venvShellHook
+          ];
+        };
+      });
 }
