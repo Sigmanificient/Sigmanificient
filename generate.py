67ebd89eb1c0409b5635a0a47d53277d5820f91d
SilentJMA <kilwanitro@gmail.com>
2023-09-22 17:19:45 +0200

    Update generate.py
    Function Decomposition: The code has been divided into functions with clear responsibilities, making it more modular and easier to read.
    
    Data Flow: The flow of data has been improved by separating data retrieval, data extraction, and README generation into separate functions.
    
    Readability: Variable and function names have been updated to be more descriptive.
    
    Code Comments: While not included in this code, consider adding comments to further explain specific sections or logic if needed.
    
    Error Handling: You might want to add error handling for cases where network requests fail or regular expressions don't match to make the script more robust.

--- generate.py
+++ generate.py
@@ -3,45 +3,49 @@ from datetime import datetime
 
 import requests
 
+# Constants
 DATE_OF_BIRTH = datetime(year=2001, month=12, day=11)
 HTML_FILTER = r'(<.*>)|(\s{2,})|(\{[\s\S]+})'
-
 FIELDS_PATTERNS = {
     'commits': r'\D(\d+)\s?Commits',
     'pr': r'\D(\d+)\s?Pull\D*\sopened',
     'issues': r'\D(\d+)\sIssues\D*\sopened',
     'streak': r'\DBest\sstreak\s(\d+)'
 }
-
 URL = (
     "https://metrics.lecoq.io/Sigmanificient"
     "?template=classic&isocalendar=1&isocalendar.duration=half-year"
     "&config.timezone=Europe%2FParis"
 )
 
-
 def compute_age():
     today = datetime.now()
     return int((today - DATE_OF_BIRTH).days / 365)
 
+def fetch_metrics_data(url):
+    response = requests.get(url)
+    return re.sub(HTML_FILTER, '', response.text)
 
-def main():
-    response = requests.get(URL)
-    filtered_response = re.sub(HTML_FILTER, '', response.text)
+def extract_metrics_data(filtered_text):
+    data = {}
+    for key, pattern in FIELDS_PATTERNS.items():
+        match = re.search(pattern, filtered_text)
+        if match:
+            data[key] = match.group(1)
+    return data
 
-    data = {
-        key: re.search(pattern, filtered_response)[1]
-        for key, pattern in FIELDS_PATTERNS.items()
-    }
-
-    data['age'] = compute_age()
-
+def generate_readme(data):
     with open('base.md', 'r') as f:
         base = f.read()
 
     with open('README.md', 'w') as f:
         f.write(base.format(**data))
 
+def main():
+    filtered_text = fetch_metrics_data(URL)
+    data = extract_metrics_data(filtered_text)
+    data['age'] = compute_age()
+    generate_readme(data)
 
 if __name__ == '__main__':
     main()
